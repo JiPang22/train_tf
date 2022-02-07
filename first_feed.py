142abdada2bcbca3aa351cfc9d6bcebb4e3e4d3c
JiPang22 <wlvud6902@gmail.com>
2022-02-07 21:17:06 +0900

    first_tf

---
+++ first_feed.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32) 
+
+w = tf.Variable(tf.random_normal([1]), name = 'weight')
+b = tf.Variable(tf.random_normal([1]),name = 'bais')
+
+hypothesis = x * w + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.01)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    cost_val, w_val, b_val, _ = \
+        sess.run([cost, w, b, train],
+                feed_dict = {x: [1,2,3], y: [1,2,3]})
+    if step % 20 == 0:
+        print(step, cost_val, w_val, b_val)
